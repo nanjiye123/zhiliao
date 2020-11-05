eb21fb6a61ca6e58b8c701be4c5119ea6f016b38
nanjiye123 <nanjiye123@163.com>
2020-11-06 04:44:22 +0800

    gateway配置

--- zhiliao-gateway/src/main/java/com/zhiliao/gateway/ZhiliaoGatewayApplication.java
+++ zhiliao-gateway/src/main/java/com/zhiliao/gateway/ZhiliaoGatewayApplication.java
@@ -2,8 +2,11 @@ package com.zhiliao.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient//开启服务注册发现
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})//排除zhiliao-common中的mybatis配置
 public class ZhiliaoGatewayApplication {
 
     public static void main(String[] args) {
